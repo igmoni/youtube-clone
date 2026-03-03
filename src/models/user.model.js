9ebb5f94c906a28d33d1fc05dd70d0c3c3ef6aad
mon <mohansp119@gmail.com>
2026-03-03 14:40:13 +0530

    Made User & Video Model

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import { Schema, model } from "mongoose";
+import { jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary URL
+      required: true,
+    },
+    coverimage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    next()
+});
+
+export const User = model("User", userSchema);
