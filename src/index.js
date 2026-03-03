6bea3db7552366a0c0ae313c78398356a9ddd539
mon <mohansp119@gmail.com>
2026-03-03 08:04:25 +0530

    Utils code updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
 // 2nd approach
-connectDB();
+connectDB()
+  .then(() => {
+    try {
+      app.on("error", (err) => {
+        console.log("Error", err);
+        throw err;
+      });
+
+      app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running: ${process.env.PORT}`);
+      });
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed!", err);
+  });
 
 // 1st Approach
 //   (async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// try-catch async handler
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({ sucess: false, message: err.message });
+//   }
+// };
